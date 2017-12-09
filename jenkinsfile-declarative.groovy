57a6bdb36ba3f89de7f567b1b430b820deb130e8
Gaurav Nagar <gauravnagar@yahoo.com>
2017-12-09 14:30:26 +0530

    Added declarative pipeline

--- jenkinsfile-declarative.groovy
+++ jenkinsfile-declarative.groovy
@@ -1,23 +1,21 @@
 node {
    def mvnHome
-   stage('Checkout') { 
-      
+   stage('Checkout') {
       checkout scm
-    
       mvnHome = tool 'maven'
    }
    stage('Build jar') {
       if (isUnix()) {
-         sh "'${mvnHome}/bin/mvn' -f workspace/spring-boot-sample clean package"
+         sh "'${mvnHome}/bin/mvn'  clean package"
       } else {
-         bat(/"${mvnHome}\bin\mvn" -f workspace\spring-boot-sample clean package/)
+         bat(/"${mvnHome}\bin\mvn"  clean package/)
       }
    }
    stage('Build docker image') {
        if (isUnix()) {
          sh "docker build workspace/spring-boot-sample -t gauravnagar/trivium-my-sql"
       } else {
-         bat(/docker build workspace\\spring-boot-sample -t gauravnagar\/trivium-my-sql/)
+         bat(/docker build workspace\spring-boot-sample -t gauravnagar\trivium-my-sql/)
       }
       
    }
